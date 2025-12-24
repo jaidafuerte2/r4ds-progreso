3e178edb9790b030e6ad022901c77eec3b95c971
jaidafuerte2 <jaidafuerte2@gmail.com>
2025-12-24 00:36:21 +0000

    hasta sección 27.4

--- program/c27.R
+++ program/c27.R
@@ -182,3 +182,137 @@ df |> subset(x > 1, c(y, z))
 
 # NO REALIZADOS
 
+tb <- tibble(
+  x = 1:4,
+  y = c(10, 4, 1, 21)
+)
+tb # produce:
+# A tibble: 4 × 2
+#      x     y
+#  <int> <dbl>
+#1     1    10
+#2     2     4
+#3     3     1
+#4     4    21
+# Seleccionar por posición la primera columna
+tb[[1]] # produce:
+#> [1] 1 2 3 4
+#> Seleccionar por posición la segunda columna
+tb[[2]] # produce:
+#[1] 10  4  1 21
+# Seleccionar la columna "x"
+tb[["x"]] # produce:
+#> [1] 1 2 3 4
+#> Seleccionar la columna "x"
+tb$x # produce:
+#> [1] 1 2 3 4
+
+# Crear nuevas columnas, similar a mutate:
+tb$z <- tb$x + tb$y
+tb # produce:
+#> # A tibble: 4 × 3
+#>       x     y     z
+#>   <int> <dbl> <dbl>
+#> 1     1    10    11
+#> 2     2     4     6
+#> 3     3     1     4
+#> 4     4    21    25
+
+# Usar $ es conveniente para realizar resúmenes rápidos. Por 
+# ejemplo, si solo quiere encontrar el tamaño del diamante más 
+# grande o los posibles valores de cut, no es necesario usar 
+# summarize():
+max(diamonds$carat) # produce:
+#> [1] 5.01
+# 
+# Conocer las categorías de cut:
+levels(diamonds$cut) # produce:
+#> [1] "Fair"      "Good"      "Very Good" "Premium"   "Ideal"
+
+# pull: toma el nombre de una tabla, una varible y devuelve los
+# valores de esa variable:
+diamonds |> pull(carat) |> max() # produce:
+#> [1] 5.01
+diamonds |> pull(carat) # produce:
+#[1] 0.23 0.21 0.23 0.29 0.31 0.24 0.24 0.26 0.22 0.23 0.30 0.23 0.22
+#[14] 0.31 0.20 0.32 0.30 0.30 0.30 0.30 0.30 0.23 0.23 0.31 0.31 0.23
+#[27] 0.24 0.30 0.23 0.23 0.23 0.23 0.23 0.23 0.23 0.23 0.23 0.31 0.26
+#[40] 0.33 0.33 0.33 0.26 0.26 0.32 0.29 0.32 0.32 0.25 0.29 0.24 0.23
+#
+diamonds |> pull(cut) |> levels() # produce:
+#> [1] "Fair"      "Good"      "Very Good" "Premium"   "Ideal"
+diamonds |> pull(cut) # produce:
+#[1] Ideal     Premium   Good      Premium   Good      Very Good
+#[7] Very Good Very Good Fair      Very Good Good      Ideal    
+#[13] Premium   Ideal     Premium   Premium   Ideal     Good     
+#[19] Good      Very Good Good      Very Good Very Good Very Good
+
+df <- data.frame(x1 = 1)
+df$x 
+#> [1] 1
+# NOTA: Aunque la columna x no existe, da el resultado de x1 porque
+# r usa lo que se conoce como coincidencia parcial
+df$z # produce:
+#> NULL
+#> NOTA: Si una columna no existe, sus valores no se muestran, obvio
+#
+# En el caso de las tibles no existe la coincidencia parcial:
+tb <- tibble(x1 = 1)
+tb$x # produce:
+#> Warning: Unknown or uninitialised column: `x`.
+#> NULL
+tb$z # produce:
+#> Warning: Unknown or uninitialised column: `z`.
+#> NULL
+
+l <- list(
+  a = 1:3, 
+  b = "a string", 
+  c = pi, 
+  d = list(-1, -5)
+)
+
+str(l[1:2]) # produce:
+#> List of 2
+#>  $ a: int [1:3] 1 2 3
+#>  $ b: chr "a string"
+l[1:2] # produce:
+#$a
+#[1] 1 2 3
+#
+#$b
+#[1] "a string"
+# NOTA: se usa la función str()para que el formato salga más bonito
+str(l[1]) # produce:
+#> List of 1
+#>  $ a: int [1:3] 1 2 3
+#
+str(l[4]) # produce:
+#> List of 1
+#>  $ d:List of 2
+#>   ..$ : num -1
+#>   ..$ : num -5
+# NOTA: Si se usan corchetes simples, lo que returna es una lista
+
+# Extraer los elementos de una lista con corchetes dobles
+str(l[[1]]) # produce:
+#>  int [1:3] 1 2 3
+#
+str(l[[4]]) # produce:
+#> List of 2
+#>  $ : num -1
+#>  $ : num -5
+# NOTA: Aquí hay dos niveles de lista por lo que los corchetes dobles
+# devuelven sólo un nivel de la lista
+#
+str(l$a) # produce:
+#>  int [1:3] 1 2 3
+
+########################
+###
+### 27.3.4 Ejercicios
+###
+########################
+
+# NO REALIZADOS
+
