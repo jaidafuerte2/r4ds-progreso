4663727a4c6ac65cefaa1120fba0c30ed2e4fd9c
jaidafuerte2 <jaidafuerte2@gmail.com>
2026-01-06 14:24:32 +0000

    quarto cap 3 final round 2

--- obesity-risk/R/obesity4.R
+++ obesity-risk/R/obesity4.R
@@ -1,65 +1,112 @@
+library(tidyverse)
 source("r4ds-progreso/obesity-risk/R/load_data.R")
 #View(obesity)
 getwd()
-# Diagrama de dispersión qué relaciona el imc con la edad tomando
-# en cuenta el tipo de obesidad
+
+# Imprimir el obesity-risk data set
+obesity # produce:
+# A tibble: 20,758 × 20
+#     id Gender   Age Height Weight family_history_with_overweight
+#  <dbl> <chr>  <dbl>  <dbl>  <dbl>                          <dbl>
+#1     0 Male    24.4   1.70   81.7                              1
+#2     1 Female  18     1.56   57                                1
+#3     2 Female  18     1.71   50.2                              1
+#4     3 Female  21.0   1.71  131.                               1
+
+# glimpse() sirve para mostrar todas las columnas
+glimpse(obesity) # produce:
+#Rows: 20,758
+#Columns: 20
+#$ id                              <dbl> 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10…
+#$ Gender                          <chr> "Male", "Female", "Female", "Fem…
+#$ Age                             <dbl> 24.44301, 18.00000, 18.00000, 20…
+#$ Height                          <dbl> 1.699998, 1.560000, 1.711460, 1.…
+#$ Weight                          <dbl> 81.66995, 57.00000, 50.16575, 13…
+#$ family_history_with_overweight  <dbl> 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,…
+#$ frequent_high_calorie_food      <dbl> 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,…
+
+# Filtrar por género Male
+obesity |>
+  filter(Gender == "Male")
+# Filtrar por género Female
+obesity |>
+  filter(Gender == "Female")
+
+# Contar tipo de obesidad por género ordenado en orden descendente
+obesity |>
+  count(Gender, tipo_obesidad, sort = TRUE)
+# Contar tipo de obesidad por género
+obesity |>
+  count(Gender, tipo_obesidad) # produce:
+# A tibble: 12 × 3
+#  Gender tipo_obesidad        n
+#  <chr>  <fct>            <int>
+#1 Female Desnutrición      1532
+#2 Female Peso normal       1886
+#3 Female Sobrepeso         1749
+#4 Female Obesidad grado 1  1195
+#5 Female Obesidad grado 2   914
+#6 Female Obesidad grado 3  3146
+#7 Male   Desnutrición       876
+#8 Male   Peso normal       1640
+#9 Male   Sobrepeso         2991
+#10 Male   Obesidad grado 1  1923
+#11 Male   Obesidad grado 2  2799
+#12 Male   Obesidad grado 3   107
+
+# Graficar un diagrama de dispersión que relaciona imc con actividad 
+# física y género
 ggplot(
   data = obesity,
-  mapping = aes(x = imc, y = Age, color = tipo_obesidad)
+  mapping = aes(x = physical_activity_frequency, y = imc, 
+                color = Gender)
 ) +
   geom_point() + 
   geom_smooth(method = "lm")
 
-# Diagrama de dispersión qué relaciona el imc con la edad tomando
-# en cuenta el tipo de obesidad; también tiene etiquetas 
-ggplot(
-  data = obesity,
-  mapping = aes(x = imc, y = Age)
-) +
-  geom_point(mapping = aes(color = tipo_obesidad, shape = tipo_obesidad)) + 
-  geom_smooth(method = "lm") +
-  labs (
-    title = "Edad e IMC",
-    subtitle = "Edad según IMC",
-    x = "IMC (Kg/m^2)", y = "Edad en años",
-    color = "Tipo de obesidad", shape = "Tipo de obesidad" 
-  )
+# Seleccionar Gender, imc, Age y tipo_obesidad
+obesity |>
+  select(Gender, Age, imc, tipo_obesidad)
 
-# Gráfico de barras para tipo de obesidad
-ggplot(obesity, aes(x = fct_infreq(tipo_obesidad))) +
-  geom_bar()
+obesity |> 
+  group_by(Gender, tipo_obesidad) |> 
+  summarize(
+    avg_imc = mean(imc, na.rm = TRUE)
+  ) # produce:
+# A tibble: 12 × 3
+# Groups:   Gender [2]
+#  Gender tipo_obesidad    avg_imc
+#  <chr>  <fct>              <dbl>
+#1 Female Desnutrición        17.3
+#2 Female Peso normal         21.8
+#3 Female Sobrepeso           27.1
+#4 Female Obesidad grado 1    32.0
 
-# Histograma de edad
-ggplot(obesity, aes(x = Age)) +
-  geom_histogram(binwidth = 1)
+obesity |> 
+  group_by(Gender, tipo_obesidad) |> 
+  summarize(
+    n_imc = n()
+  ) # produce:
+# A tibble: 12 × 3
+# Groups:   Gender [2]
+#  Gender tipo_obesidad    n_imc
+#  <chr>  <fct>            <int>
+#1 Female Desnutrición      1532
+#2 Female Peso normal       1886
+#3 Female Sobrepeso         1749
+#4 Female Obesidad grado 1  1195
+#5 Female Obesidad grado 2   914
+#6 Female Obesidad grado 3  3146
+#7 Male   Desnutrición       876
+#8 Male   Peso normal       1640
+#9 Male   Sobrepeso         2991
+#10 Male   Obesidad grado 1  1923
+#11 Male   Obesidad grado 2  2799
+#12 Male   Obesidad grado 3   107
 
-# Gráfico de densidad de edad
-ggplot(obesity, aes(x = Age)) +
-  geom_density()
+obesity |>
+  filter(Gender == "Male" && tipo_obesidad == "Obesidad grado 3")
 
-# Diagrama de caja de Edad y tipo de obesidad
-ggplot(obesity, aes(x = tipo_obesidad, y = Age)) +
-  geom_boxplot()
-
-# Gráfico de densidad de Edad y tipo de obesidad
-ggplot(obesity, aes(x = Age, color = tipo_obesidad)) +
-  geom_density(linewidth = 0.75)
-
-# Gráfico de densidad de Edad y tipo de obesidad con relleno y opacidad
-ggplot(obesity, aes(x = Age, color = tipo_obesidad, 
-                    fill = tipo_obesidad)) +
-  geom_density(alpha = 0.5)
-
-# Diagrama de dispersión que relaciona edad e imc
-ggplot(obesity, aes(x = Age, y = imc)) +
-  geom_point()
-
-# Relacionar género, Edad con imc y tipo de obesidad con un diagrama de 
-# dispersión
-ggplot(obesity, aes(x = imc, y = Age)) +
-  geom_point(aes(color = tipo_obesidad, shape = Gender))
-
-# Facetar por género las variables imc, Age y tipo de obesidad
-ggplot(obesity, aes(x = imc, y = Age)) +
-  geom_point(aes(color = tipo_obesidad, shape = tipo_obesidad)) +
-  facet_wrap(~Gender)
+obesity |>
+  filter(tipo_obesidad == "Obesidad grado 3") |>
+  filter(Gender == "Male")
