2a1fff45321c581163db34069bf0e6ec174d18ef
jaidafuerte2 <jaidafuerte2@gmail.com>
2026-02-02 15:17:58 +0000

    hasta tranformacion come refrigerios del obesity latin

--- obesity-latin/R/load_data.R
+++ obesity-latin/R/load_data.R
@@ -16,13 +16,164 @@ obesity <- obesity |>
 obesity <- obesity |>
   mutate(
     tipo_obesidad = case_when(
-      imc < 18.5 ~ "Desnutrición",
+      imc < 18.5 ~ "Desnutricion",
       imc < 25 ~ "Peso normal",
-      imc < 30 ~ "Sobrepesso",
+      imc < 30 ~ "Sobrepeso",
       imc < 35 ~ "Obesidad grado 1",
       imc < 40 ~ "Obesidad grado 2",
       imc >= 40 ~ "Obesidad grado 3",
       TRUE ~ NA_character_
     )
   )
-#View(obesity)
+
+# Renombrar a las varibles de la data set de obesidad para que sean más
+# legibles en español
+obesity <- obesity |>
+  rename(
+    tiene_familiares_obesos = family_history_with_overweight,
+    come_comida_muy_calorica = FAVC,
+    come_vegetales = FCVC,
+    comidas_principales = NCP,
+    come_refrigerios = CAEC,
+    fuma = SMOKE,
+    agua_diaria = CH2O,
+    toma_bebidas_calóricas = SCC,
+    actividad_fisica = FAF,
+    uso_tecnologia = TUE,
+    consume_alcohol = CALC,
+    medio_transporte = MTRANS
+  )
+# NOTA: Es mejor que las variables numéricas no tengan verbo, las 
+# variables dicotomicas es mejor nombrarlas al inicio con verbos y
+# las categorías a veces es bueno iniciarlas con verbos como en 
+# consumo de alcohol, pero esto no siempre es ideal como en medio
+# de transporte
+
+
+# Conocer el tipo de la variable tipo de obesidad:
+#class(obesity$tipo_obesidad) # produce: "character"
+# Conocer el orden del tipo de valores de la variable tipo de obesidad
+#unique(obesity$tipo_obesidad) # produce:
+#[1] "Peso normal"      "Sobrepeso"       "Obesidad grado 1"
+#[4] "Desnutricion"     "Obesidad grado 2" "Obesidad grado 3"
+# Convertir la variable tipo de obesidad de character a factor:
+obesity <- obesity |>
+  mutate(
+    tipo_obesidad = factor(
+      tipo_obesidad,
+      levels = c(
+        "Desnutricion",
+        "Peso normal",
+        "Sobrepeso",
+        "Obesidad grado 1",
+        "Obesidad grado 2",
+        "Obesidad grado 3"
+      )
+    )
+  )
+#class(obesity$tipo_obesidad) # produce: factor
+#unique(obesity$tipo_obesidad) # produce:
+#[1] Peso normal      Sobrepeso        Obesidad grado 1 Desnutricion    
+#[5] Obesidad grado 2 Obesidad grado 3
+
+# Convertir género a factor
+obesity <- obesity |>
+  mutate(
+    Gender = factor(
+      Gender,
+      levels = c("Male", "Female")
+    )
+  )
+#class(obesity$Gender) # produce: "factor"
+
+# Traducir los valores de la variable de historia familiar de obesidad 
+obesity <- obesity |>
+  mutate(
+    tiene_familiares_obesos = recode(
+      tiene_familiares_obesos,
+      "yes" = "si",
+      "no" = "no"
+    )
+  )
+#class(obesity$tiene_familiares_obesos) # produce: "character"
+
+# Cambiar el tipo de la variable de historia familiar de obesidad
+#a factor
+obesity <- obesity |>
+  mutate(
+    tiene_familiares_obesos = factor(
+      tiene_familiares_obesos,
+      levels = c("si", "no")
+    )
+  )
+#class(obesity$tiene_familiares_obesos) # produce: "factor"
+
+# Traducir los valores de la variable comida muy calórica
+obesity <- obesity |>
+  mutate(
+    come_comida_muy_calorica = recode(
+      come_comida_muy_calorica,
+      "yes" = "si",
+      "no" = "no"
+    )
+  )
+#class(obesity$come_comida_muy_calorica) # produce: "character"
+
+# Cambiar el tipo de la variable comida muy calórica a factor
+obesity <- obesity |>
+  mutate(
+    come_comida_muy_calorica = factor(
+      come_comida_muy_calorica,
+      levels = c("si", "no")
+    )
+  )
+#class(obesity$come_comida_muy_calorica) # produce: "factor"
+
+# Conocer los valores de la variable come vegetales
+#unique(obesity$come_vegetales) # produce:
+#[1] 2.000000 3.000000 1.000000 2.450218 2.880161 2.008760 2.596579
+#[8] 2.591439 2.392665 1.123939 2.027574 2.658112 2.886260 2.714447
+#[15] 2.750715 1.492500 2.205439 2.059138 2.310423 2.823179 2.052932
+#[22] 2.596364 2.767731 2.815157 2.737762 2.568063 2.524428 2.971574
+# Conocer el tipo de la variable come vegetales
+#class(obesity$come_vegetales) # produce : "numeric"
+# NOTA: Es posible que sean valores por semana, el manual no lo 
+# indica. "numeric" es double o de doble precisión es decir que puede
+# tener decimales
+
+# Conocer los valores de la variable comidas principales
+#unique(obesity$comidas_principales) # produce:
+#[1] 2.000000 3.000000 1.000000 2.450218 2.880161 2.008760 2.596579
+#[8] 2.591439 2.392665 1.123939 2.027574 2.658112 2.886260 2.714447
+#[15] 2.750715 1.492500 2.205439 2.059138 2.310423 2.823179 2.052932
+#[22] 2.596364 2.767731 2.815157 2.737762 2.568063 2.524428 2.971574
+# Conocer el tipo de la varaible comidas principales
+#class(obesity$comidas_principales) # produce: "numeric"
+
+# Conocer los valores que tiene la variable come refrigerios
+#unique(obesity$come_refrigerios) # produce: 
+#[1] "Sometimes"  "Frequently" "Always"     "no"   
+# Traducir los valores de la variable come refrigerios 
+obesity <- obesity |>
+  mutate(
+    come_refrigerios = recode(
+      come_refrigerios,
+      "Sometimes" = "ocasional",
+      "Frequently" = "frecuente",
+      "no" = "no",
+      "Always" = "siempre"
+    )
+  )
+#unique(obesity$come_refrigerios) # produce:
+#[1] "ocasional" "frecuente" "siempre"   "no"
+#class(obesity$come_refrigerios) # produce: "character"
+
+# Cambiar el tipo de la variable come refrigerios
+obesity <- obesity |>
+  mutate(
+    come_refrigerios = factor(
+      come_refrigerios, 
+      levels = c("ocasional", "frecuente", "nunca", "siempre")
+    )
+  )
+#class(obesity$come_refrigerios) # produce: "factor
--- obesity-latin/R/obesity3.R
+++ obesity-latin/R/obesity3.R
@@ -1,10 +1,24 @@
+########################################################
+##                                                    ##
+##                   PUNTOS IMPORTANTES               ##
+##                                                    ##
+########################################################
+
 source("r4ds-progreso/obesity-latin/R/load_data.R")
+library(tidyverse)
+
+############################
+##
+##       Visualizaciones
+##
+############################
+
 
 # Visualizar barras de tipo de obesidad en orden descendente
 ggplot(obesity, aes(x = fct_infreq(tipo_obesidad))) +
   geom_bar()
 
-# Diagrama de densidad que relaciona la edad con el tipode obesidad
+# Diagrama de densidad que relaciona la edad con el tipo de obesidad
 ggplot(obesity, aes(x = Age, color = tipo_obesidad)) +
   geom_density(linewidth = 0.75)
 
@@ -13,6 +27,13 @@ ggplot(obesity, aes(x = Age, color = tipo_obesidad)) +
 ggplot(obesity, aes(x = Age, color = tipo_obesidad, 
                     fill = tipo_obesidad)) +
   geom_density(alpha = 0.5)
+
+########################
+##
+## Transformaciones
+##
+########################
+
 
 # Los nombres de las variables y otros se separan con guiones bajos
 # como en python, no con guiones medios como en lisp
@@ -31,4 +52,106 @@ obesity_renamed[1:4,] # produce:
 #2 Female    21   1.52    56 yes                    no        3     3 Some…
 #3 Male      23   1.8     77 yes                    no        2     3 Some…
 #4 Male      27   1.8     87 no                     no        3     3 Some…
+
+# Reposicionar imc y tipo de obesidad al inicio de la tabla
+obesity_relocated <- obesity |>
+  relocate(imc, tipo_obesidad)
+obesity_relocated # produce:
+# A tibble: 2,111 × 19
+#   imc tipo_obesidad Gender   Age Height Weight family_history_with_ov…¹
+#   <dbl> <chr>         <chr>  <dbl>  <dbl>  <dbl> <chr>                   
+#1  24.4 Peso normal   Female    21   1.62   64   yes                     
+#2  24.2 Peso normal   Female    21   1.52   56   yes                     
+#3  23.8 Peso normal   Male      23   1.8    77   yes                     
+#4  26.9 Sobrepesso    Male      27   1.8    87   no   
+
+# Agrupar por tipo de obesidad y género y resumir por imc y edad
+obesity |>
+  group_by(tipo_obesidad, Gender) |>
+  summarise(
+    med_imc = median(imc, na.rm = TRUE),
+    med_age = median(Age, na.rm = TRUE),
+    .groups = "drop"
+  ) # produce:
+# A tibble: 12 × 4
+#  tipo_obesidad    Gender med_imc med_age
+#  <chr>            <chr>    <dbl>   <dbl>
+#1 Desnutrición     Female    17.5    19.9
+#2 Desnutrición     Male      17.6    18  
+#3 Obesidad grado 1 Female    31.9    23  
+#4 Obesidad grado 1 Male      32.7    22.9
+#5 Obesidad grado 2 Female    39.0    25.9
+#6 Obesidad grado 2 Male      36.5    27.6
+#7 Obesidad grado 3 Female    42.8    22.8
+#8 Obesidad grado 3 Male      49.5    18  
+#9 Peso normal      Female    22.0    21  
+#10 Peso normal      Male      22.5    21  
+#11 Sobrepesso       Female    26.7    22.8
+#12 Sobrepesso       Male      27.2    22.7
+
+#################################
+##
+##           Estilo
+##
+#################################
+
+# Los nombres usados antes de "<-" y en mutate deben tener solo 
+# letras minúsculas, numeros y se separa palabras con guiones bajos 
+# "_"
+
+# Los operadores siempre van con espacios menos el gno de potencia
+z <- (2 + 4)^2 / 12 
+
+
+# Estilo para nombres de archivos:
+# 1. Evitar espacios, símbolos y caracteres especiales.
+# 2. Los nombres deben describir lo que hay en el archivo
+# 3. Los nombres deben funcionar bien con el orden predeterminado, 
+# incluso puede comenzar con números para que se coloquen en el orden
+# correcto. Ejemplo:
+# - Mal:
+#alternative model.R
+#code for exploratory analysis.r
+#finalreport.qmd
+#FinalReport.qmd
+#fig 1.png
+#Figure_02.png
+#model_first_try.R
+#run-first.r
+#temp.txt
+# Bien:
+#01-load-data.R
+#02-exploratory-analysis.R
+#03-model-approach-1.R
+#04-model-approach-2.R
+#fig-01.png
+#fig-02.png
+#report-2022-03-20.qmd
+#report-2022-04-02.qmd
+#report-draft-notes.txt
+
+# Conocer el directorio actual de trabajo
+getwd() # produce:
+#[1] "/cloud/project"
+
+####################################
+##
+##         Importar Datos
+##
+####################################
+
+# Cambiar el tipo de la variable género
+obesity_new_types <- obesity |>
+  mutate(
+    Gender = factor(Gender)
+  )
+obesity_new_types[1:4,]  # produce:
+# A tibble: 2,111 × 19
+#  Gender   Age Height Weight family_history_with_over…¹ FAVC   FCVC   NCP
+#  <fct>  <dbl>  <dbl>  <dbl> <chr>                      <chr> <dbl> <dbl>
+#1 Female    21   1.62   64   yes                        no        2     3
+#2 Female    21   1.52   56   yes                        no        3     3
+#3 Male      23   1.8    77   yes                        no        2     3
+#4 Male      27   1.8    87   no                         no        3     3
+
 
